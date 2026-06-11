332664446214e428286b207c5ce9d02a33a41608
Денис Подгорнов <estate737@gmail.com>
2026-06-11 20:23:33 +0300

    refactor(homework9): drop CreateOrderDealActivity in favor of standard deal activity

--- local/activities/custom/createorderdealactivity/lang/ru/.description.php
+++
@@ -1,5 +0,0 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-$MESS['CREATEORDERDEAL_DESCR_NAME'] = 'Создать сделку по заявке';
-$MESS['CREATEORDERDEAL_DESCR_DESCR'] = 'Создаёт сделку CRM, привязанную к компании, с названием из вида работ и суммой из заявки';
-$MESS['CREATEORDERDEAL_DESCR_CATEGORY'] = 'Собственные активити';
-$MESS['CREATEORDERDEAL_DESCR_FIELD_DEAL_ID'] = 'ID сделки';
